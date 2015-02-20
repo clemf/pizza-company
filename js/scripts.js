310fa3d59372efdc2f9fdb2679abbbad8c0d439b
Clem Freeman <clemfree@gmail.com>
2015-02-20 11:20:35 -0800

    Add tests and logic for all pizza sizes

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,14 @@ var Pizza = {
   slices: function() {
     if (this.diameter >= 16) {
       return 16;
+    } else if (this.diameter >= 14) {
+      return 12;
+    } else if (this.diameter >= 12) {
+      return 8;
+    } else if (this.diameter >= 9.5) {
+      return 6;
+    } else {
+      return 4;
     }
   }
 
--- spec/specs.js
+++ spec/specs.js
@@ -16,9 +16,28 @@ describe('Pizza', function() {
     expect(testPizza.topping).to.equal("pepperoni");
   });
 
-  it('returns the correct number of slices for a 16" pizza', function() {
+  it('returns the correct number of slices for an x-large pizza', function() {
     testPizza.setDiameter(16);
     expect(testPizza.slices()).to.equal(16);
   });
 
+  it('returns the correct number of slices for a large pizza', function() {
+    testPizza.setDiameter(14);
+    expect(testPizza.slices()).to.equal(12);
+  });
+
+  it('returns the correct number of slices for a medium pizza', function() {
+    testPizza.setDiameter(12);
+    expect(testPizza.slices()).to.equal(8);
+  });
+
+  it('returns the correct number of slices for a small pizza', function() {
+    testPizza.setDiameter(9.5);
+    expect(testPizza.slices()).to.equal(6);
+  });
+
+  it('returns the correct number of slices for a personal pizza', function() {
+    testPizza.setDiameter(6.5);
+    expect(testPizza.slices()).to.equal(4);
+  });
 });
