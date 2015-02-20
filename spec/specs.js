92dedc43becd8fd2db543da4c0049ece1d70436f
Clem Freeman <clemfree@gmail.com>
2015-02-20 10:18:55 -0800

    Add test for setTopping

--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,14 @@
 describe('Pizza', function() {
 
   it('sets the pizza size property', function() {
-    testPizza = Object.create(Pizza);
+    var testPizza = Object.create(Pizza);
     testPizza.setSize(13);
     expect(testPizza.size).to.equal(13);
   });
 
+  it('sets the pizza topping property', function() {
+    var testPizza = Object.create(Pizza);
+    testPizza.setTopping("pepperoni");
+    expect(testPizza.topping).to.equal(pepperoni);
+  });
 });
